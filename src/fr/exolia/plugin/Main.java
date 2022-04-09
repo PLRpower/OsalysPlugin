8c0f45bfa0bd92872e90bcd3d3d7ee47bf734245
PLR <paul.thomas.stras@gmail.com>
2022-04-09 17:23:21 +0200

    Petite modification

--- src/fr/exolia/plugin/Main.java
+++ src/fr/exolia/plugin/Main.java
@@ -1,12 +1,8 @@
 package fr.exolia.plugin;
 
 import fr.exolia.plugin.commands.PublicCommands;
-import fr.exolia.plugin.listeners.PlayerChat;
 import fr.exolia.plugin.mysql.MySQL;
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-
 import java.util.ArrayList;
 import java.util.UUID;
 
@@ -15,12 +11,14 @@ public class Main extends JavaPlugin {
     private static Main instance;
     public MySQL mysql = new MySQL();
     public ArrayList<UUID> moderateurs = new ArrayList<>();
+    public static String PrefixInfo = "§aExolia §f» §7";
+    public static String PrefixError = "§2Exolia §f» §c";
+    public static String PrefixAnnounce = "§2Exolia §f» §a";
 
     @Override
     public void onEnable() {
-        System.out.println("§2Exolia §f» §aActivation du plugin en cours ...");
+        System.out.println(PrefixInfo + "Activation du plugin en cours ...");
         instance = this;
-        registerEvents();
         mysql.connect("45.76.45.183", 3306, "site", "PLR", "@4qkVi06&");
         getCommand("site").setExecutor(new PublicCommands());
         getCommand("vote").setExecutor(new PublicCommands());
@@ -28,19 +26,14 @@ public class Main extends JavaPlugin {
         getCommand("discord").setExecutor(new PublicCommands());
         getCommand("hub").setExecutor(new PublicCommands());
         getCommand("hoteldevente").setExecutor(new PublicCommands());
-        System.out.println("§2Exolia §f» §aLe plugin s'est correctement activé.");
-    }
-
-    private void registerEvents() {
-        PluginManager pm = Bukkit.getPluginManager();
-        pm.registerEvents(new PlayerChat(), this);
+        System.out.println(PrefixAnnounce + "Le plugin s'est correctement activé.");
     }
 
     @Override
     public void onDisable() {
-        System.out.println("§2Exolia §f» §aDésactivation du plugin en cours ...");
+        System.out.println(PrefixInfo + "Désactivation du plugin en cours ...");
         mysql.disconnect();
-        System.out.println("§2Exolia §f» §aLe plugin s'est correctement désactivé.");
+        System.out.println(PrefixAnnounce + "Le plugin s'est correctement désactivé.");
     }
 
 
